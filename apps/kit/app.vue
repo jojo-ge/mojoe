51232174315d0a5c338efbd99d342b1d6101e164
Joseph George <joseph@benda.app>
2025-02-15 18:16:25 +1100

    atoms molecules and organisms menus

--- apps/kit/app.vue
+++ apps/kit/app.vue
@@ -1,42 +1,63 @@
 <script setup lang="ts">
-const menuOptions = [
+const menuOptions = {
+  atoms: [
+    {
+      label: "Typography",
+      value: "KitTypography",
+    },
+    {
+      label: "Buttons",
+      value: "KitButtons",
+    },
+    {
+      label: "Chips",
+      value: "KitChips",
+    },
+    {
+      label: "Loaders",
+      value: "KitLoaders",
+    },
+    {
+      label: "Text input",
+      value: "KitInputText",
+    },
+    {
+      label: "Checkboxes",
+      value: "KitInputCheckbox",
+    },
+    {
+      label: "Select input",
+      value: "KitInputSelect",
+    },
+    {
+      label: "Switch input",
+      value: "KitInputSwitch",
+    },
+    {
+      label: "Cards",
+      value: "KitCards",
+    },
+  ],
+  molecules: [],
+  organisms: [],
+};
+
+const mainTabs = [
   {
-    label: "Typography",
-    value: "KitTypography",
+    label: "Atoms",
+    value: "atoms",
   },
   {
-    label: "Buttons",
-    value: "KitButtons",
+    label: "Molecules",
+    value: "molecules",
   },
   {
-    label: "Chips",
-    value: "KitChips",
-  },
-  {
-    label: "Loaders",
-    value: "KitLoaders",
-  },
-  {
-    label: "Text input",
-    value: "KitInputText",
-  },
-  {
-    label: "Checkboxes",
-    value: "KitInputCheckbox",
-  },
-  {
-    label: "Select input",
-    value: "KitInputSelect",
-  },
-  {
-    label: "Switch input",
-    value: "KitInputSwitch",
-  },
-  {
-    label: "Cards",
-    value: "KitCards",
+    label: "Organisms",
+    value: "organisms",
   },
 ];
+
+const activeMainTab = ref("atoms");
 
 const activeTab = ref("KitTypography");
 const activeComponent = shallowRef();
@@ -48,15 +69,46 @@ const setActiveTab = (componentName: string) => {
   );
 };
 
+const menuTitle = computed(() => {
+  return mainTabs.find((e) => e.value === activeMainTab.value)?.label;
+});
+
 setActiveTab("KitTypography");
 </script>
 
 <template>
   <div class="pt-16 grid grid-cols-5 gap-6 mx-auto container">
+    <UIAtHeading
+      size="h1"
+      styleSize="h1"
+      text="MoJoe Design System"
+      class="font-display text-primary col-span-5"
+    />
+    <div class="col-span-5">
+      <ul class="flex gap-6">
+        <li
+          v-for="tab in mainTabs"
+          :key="tab.value"
+          @click="activeMainTab = tab.value"
+        >
+          <UIAtChip
+            class="cursor-pointer"
+            :text="tab.label"
+            :variant="activeMainTab === tab.value ? 'primary' : 'outline'"
+          />
+        </li>
+      </ul>
+    </div>
     <UIAtCard class="h-fit col-span-1">
+      <UIAtHeading
+        size="h3"
+        styleSize="h3"
+        :text="menuTitle"
+        class="text-primary"
+      />
       <ul>
         <li
-          v-for="option in menuOptions"
+          v-for="option in menuOptions[<keyof typeof menuOptions>activeMainTab]"
           :key="option.value"
           class="hover:bg-primary p-2 hover:text-white cursor-pointer"
           :class="{ 'text-primary font-semibold': activeTab === option.value }"
@@ -66,9 +118,11 @@ setActiveTab("KitTypography");
         </li>
       </ul>
     </UIAtCard>
-    <UIAtCard class="w-full col-span-4" variant="surface">
-      <component :is="activeComponent" />
-    </UIAtCard>
+    <div class="w-full col-span-4">
+      <UIAtCard variant="surface" class="w-full">
+        <component :is="activeComponent" />
+      </UIAtCard>
+    </div>
   </div>
   <div class="fixed inset-0 -top-200 -z-200 flex justify-center items-start">
     <div
